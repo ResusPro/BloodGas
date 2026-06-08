8b8af6c3b4cc8aff8b1779136453d90502c33461
ResusPro <dr.nick.foster80@gmail.com>
2026-06-08 04:21:17 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,17 @@
-const CACHE_NAME = 'bloodgas-v1-rc2';
+const CACHE_NAME = 'bloodgas-v1';
 
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './logo.png',
-  './manifest.json',
-  './resuspro-icon-192.png',
-  './resuspro-icon-512.png'
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(FILES_TO_CACHE))
   );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
